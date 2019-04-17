ec342f9c8aa147e09d5eb5d05f8dde013e32ec89
zichd <zichd@seznam.cz>
2019-04-17 13:53:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='dznameless',
-    version='0.0.1',
+    version='0.0.2',
     license='BSD 2-Clause License',
     description='An example package. Generated with cookiecutter-pylibrary.',
     long_description='%s\n%s' % (
